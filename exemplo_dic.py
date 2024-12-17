82c59404d5327981f328592d97a538ba641fa3cf
YanZN <yanrobim@gmail.com>
2024-12-17 19:12:28 -0300

    como criar, desenvolver um dicionario. Bonus: informações sobre o json

---
+++ exemplo_dic.py
@@ -0,0 +1,24 @@
+# from typing import Dict, Optional, Any
+
+# import json
+
+# produto1: Dict [str, Any]  = {
+#     "nome": "Tenis",
+#     "quantidade": 39,
+#     "Preço": 10.38,
+#     "disponibilidade": True
+# }
+
+# produto2: dict = {
+#     "nome": "Televisão",
+#     "quantidade": 10,
+#     "Preço": 70,
+#     "disponibilidade": False
+# }
+
+# carrinho: list = []
+# carrinho.append(produto1)
+# carrinho.append(produto2)
+
+# carrinho_json = json.dumps(carrinho)
+# print(carrinho_json)
