971610c940bc98c166835b2878aec4eb90a991aa
YanZN <yanrobim@gmail.com>
2024-12-17 19:13:44 -0300

    aprendendo mais sobre criar uma lista

---
+++ main.py
@@ -0,0 +1,20 @@
+#Exemplo Lista
+
+# produto: str = "tenis"
+# produto_2: str = "camisa"
+# produto_3: str = "controle"
+
+# produtos: list = []
+# produtos.append(produto)
+# produtos.append(produto_2)
+# produtos.append(produto_3)
+# produtos.pop()
+
+# print(produtos)
+
+
+# numeros = []
+# numeros.extend(range(0,5))
+# print(numeros)
+
+#extend tem a função de add range
